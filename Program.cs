b8d7840566db077817e93218ebf17ff6ba3ae629
Luciano-190 <lucianopaulinu2019@gmail.com>
2026-07-06 17:21:37 -0300

    Atividade 5

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+        int i = 0, par = 0;
+        for (i = 0; i < num.Length; i++)
+        {
+            Console.WriteLine("Informe o primeiro número");
+            num[i] = int.Parse(Console.ReadLine());
+            if(num[i] % 2 == 0)
+            {
+                Console.WriteLine($" O número {num[i]} é par!!");
+                par++;
+            }
+        }
+        Console.WriteLine($"Você tem um total de {par} números pares!!");
+    }
+}
