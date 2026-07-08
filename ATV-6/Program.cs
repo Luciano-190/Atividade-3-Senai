d7c7c1f07974f21143db54e6ad1c5fcc1f66afa9
Luciano-190 <lucianopaulinu2019@gmail.com>
2026-07-08 14:28:33 -0300

    Atividade-6

---
+++ ATV-6/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int i = 0, maior = 0, menor = 999999999;
+        int[] num = new int[10];
+        for (i = 0; i < num.Length; i++)
+        {
+            Console.WriteLine($"Informe o {i + 1}º número do vetor");
+            num[i] = int.Parse(Console.ReadLine());
+        }
+        for(i = 0; i < num.Length; i++)
+        {
+            if (maior < num[i])
+            {
+                maior = num[i];
+            }
+            if(menor > num[i])
+            {
+                menor = num[i];
+            }
+        }
+        Console.WriteLine($"O seu maior número e {maior}, e o menor número e o {menor}");
+    }
+}
